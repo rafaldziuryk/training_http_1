bbf31c8615b3d32e87b2ceecff6f4e65cd76227a
rafaldziuryk <rafal.dziuryk@appvinio.com>
2022-06-21 13:21:39 +0200

    update

---
+++ lib/step_5/cat_fact.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cat_fact.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CatFact _$CatFactFromJson(Map<String, dynamic> json) => CatFact(
+      fact: json['fact'] as String,
+      length: json['length'] as int,
+    );
+
+Map<String, dynamic> _$CatFactToJson(CatFact instance) => <String, dynamic>{
+      'fact': instance.fact,
+      'length': instance.length,
+    };
